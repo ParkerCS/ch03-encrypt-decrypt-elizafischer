37acdeba01acce01ee6e16d3151c29ae283f6b52
elizafischer <efischer@fwparker.org>
2017-02-07 11:09:08 -0600

    change1

--- ch03_assignment.py
+++ ch03_assignment.py
@@ -10,3 +10,4 @@ encrypted_message = "¿®ªªÈÙ®ÏT¤ÕEÓ¹âeCíÉÁÏº¢¡i¸
 #2 Encrypt your name and print the encrypted result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
 
 #3 Decrypt the encrypted code from part 2 to ensure that it worked properly and print the result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
+#Change
